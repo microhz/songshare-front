33250920ffbe44bdef0cd6ffa70a5dcfda23ab4d
micro <pengmi2@126.com>
2017-07-12 13:11:41 +0800

    update home

---
+++ js/ss/home.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    // $.ajax({
+    //     url:'ss/music/getRentRecommendList.do',// 跳转到 action  
+    //     data:{  
+    //         count : 20
+    //     },  
+    //     type:'post',  
+    //     cache:false,  
+    //     dataType:'json',  
+    //     success:function(data) {  
+    //         if(data.msg =="true" ){  
+    //             // view("修改成功！");  
+    //             alert("查询成功！");  
+    //         }else{  
+    //         }  
+    //     },  
+    //     error : function() {  
+    //         alert("异常！");  
+    //     } 
+    // })
+    $(".list-group").append("<li class='list-group-item'>测试标题</li>");
+})
